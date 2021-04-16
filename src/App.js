9c6919cb832919db025287b88ec663bf0635edf9
Shaine Tsou <shaineagro06@gmail.com>
2021-04-16 22:28:00 +0800

    import HomePage component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+import HomePage from './homepage.component';
 
 function App() {
   return (
-    <div>Hello World!</div>
+    <div>
+      <HomePage />
+    </div>
   );
 }
 
