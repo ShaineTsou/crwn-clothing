14c7c6a9df0566fc71ed5c190ce6d3ef074679f4
Shaine Tsou <shaineagro06@gmail.com>
2021-05-08 23:03:17 +0800

    moved the mapping function on CollectionsPreview component into CollectionsOverview component in order to make Shop component a simple non-connected component.

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,21 +1,11 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
 
-import { selectShopCollections } from '../../redux/shop/shop.selectors';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
-
-const ShopPage = ({ collections }) => (
+const ShopPage = () => (
     <div className='shop-page'>
-        {collections.map(({ id, ...otherCollectionProps }) => (
-            <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
+        <CollectionsOverview />
     </div>
 )
 
-const mapStateToProps = createStructuredSelector({
-    collections: selectShopCollections
-});
-
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
