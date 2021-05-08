e599965dfa7bb4bdc323fbd73a62123fb194c112
Shaine Tsou <shaineagro06@gmail.com>
2021-05-08 22:34:54 +0800

    pulled the collections state stored in the shop component out into the redux store

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,28 +1,21 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
-import SHOP_DATA from './shop.data';
+import { selectShopCollections } from '../../redux/shop/shop.selectors';
 
 import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 
-class ShopPage extends React.Component {
-    constructor(props) {
-        super(props);
+const ShopPage = ({ collections }) => (
+    <div className='shop-page'>
+        {collections.map(({ id, ...otherCollectionProps }) => (
+            <CollectionPreview key={id} {...otherCollectionProps} />
+        ))}
+    </div>
+)
 
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
-    
-    render() {
-        const { collections } = this.state;
-        return (
-            <div className='shop-page'>
-                {collections.map(({ id, ...otherCollectionProps }) => (
-                    <CollectionPreview key={id} {...otherCollectionProps} />
-                ))}
-            </div>
-        );
-    }
-}
+const mapStateToProps = createStructuredSelector({
+    collections: selectShopCollections
+});
 
-export default ShopPage;
+export default connect(mapStateToProps)(ShopPage);
--- src/redux/directory/directory.reducer.js
+++ src/redux/directory/directory.reducer.js
@@ -38,7 +38,7 @@ const INITIAL_STATE = {
 const directoryReducer = (state=INITIAL_STATE, action) => {
     switch(action.type) {
         default: 
-        return state;
+            return state;
     }
 };
 
