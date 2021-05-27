96e11c5a2752f07868e23ca559d6f0e287eaafe7
Shaine Tsou <shaineagro06@gmail.com>
2021-05-27 22:33:58 +0800

    created user sagas for sign up

--- src/redux/user/user.sagas.js
+++ src/redux/user/user.sagas.js
@@ -10,13 +10,15 @@ import UserActionTypes from './user.types';
 import { 
     signInSuccess,
     signInFailure,
+    signUpSuccess,
+    signUpFailure,
     signOutSuccess,
     signOutFailure
 } from './user.actions';
 
-export function* getSnapshotFromUserAuth(userAuth) {
+export function* getSnapshotFromUserAuth(userAuth, additionalData) {
     try {
-        const userRef = yield call(createUserProfileDocument, userAuth);
+        const userRef = yield call(createUserProfileDocument, userAuth, additionalData);
         const userSnapshot = yield userRef.get();
         yield put(signInSuccess({
             id: userSnapshot.id,
@@ -64,6 +66,19 @@ export function* signOut() {
     }
 };
 
+export function* signUp({ payload: { email, password, displayName }}) {
+    try {
+        const { user } = yield auth.createUserWithEmailAndPassword(email, password);
+        yield put(signUpSuccess({ user, additionalData: { displayName } }));
+    } catch(error) {
+        yield put(signUpFailure(error));
+    }
+};
+
+export function* signInAfterSignUp({ payload: { user, additionalData }}) {
+    yield getSnapshotFromUserAuth(user, additionalData);
+};
+
 export function* onGoogleSignInStart() {
     yield takeLatest(UserActionTypes.GOOGLE_SIGN_IN_START, signInWithGoogle)
 };
@@ -80,11 +95,21 @@ export function* onSignOut() {
     yield takeLatest(UserActionTypes.SIGN_OUT_START, signOut);
 };
 
+export function* onSignUpStart() {
+    yield takeLatest(UserActionTypes.SIGN_UP_START, signUp);
+};
+
+export function* onSignUpSuccess() {
+    yield takeLatest(UserActionTypes.SIGN_UP_SUCCESS, signInAfterSignUp);
+}
+
 export function* userSaga() {
     yield all([
         call(onGoogleSignInStart),
         call(onEmailSignInStart),
         call(onCheckUserSession),
-        call(onSignOut)
+        call(onSignOut),
+        call(onSignUpStart),
+        call(onSignUpSuccess)
     ]);
 };
