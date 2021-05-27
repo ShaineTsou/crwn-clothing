329318867759df7fad9f02531b4cc595c23ab1a7
Shaine Tsou <shaineagro06@gmail.com>
2021-05-27 20:43:16 +0800

    corrected the meaning of the title

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -56,7 +56,7 @@ class SignUp extends React.Component {
     render() {
         return (
             <div className='sign-up'>
-                <h2 className='title'>I do have an account</h2>
+                <h2 className='title'>I do not have an account</h2>
                 <span>Sign up with your email and password</span>
                 
                 <form className='sign-up-form' onSubmit={this.handleSubmit}>
