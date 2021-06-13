b2ed7b8997a355fc539be3d188367733e0c19d62
Shaine Tsou <shaineagro06@gmail.com>
2021-06-13 22:23:50 +0800

    use compression middleware in the production mode

--- server.js
+++ server.js
@@ -14,11 +14,11 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV === 'production') {
+    app.use(compression());
     app.use(enforce.HTTPS({ trustProtoHeader: true }));
     app.use(express.static(path.join(__dirname, 'client/build')));
 
