e5211fe7762315219aaeb5ea08505cceae6dca91
Shaine Tsou <shaineagro06@gmail.com>
2021-08-10 16:51:45 +0800

    converted custom-button into styled-component, and modified the styling inside CollectionItemContainer

--- client/src/components/custom-button/custom-button.component.jsx
+++ client/src/components/custom-button/custom-button.component.jsx
@@ -1,17 +1,11 @@
 import React from 'react';
 
-import './custom-button.styles.scss';
+import { CustomButtonContainer } from './custom-button.styles';
 
-// Presentational Component (i.e. functional component with no state)
-const CustomButton = ({ children, googleSignIn, inverted, ...otherProps }) => (
-    <button 
-        className={`
-            ${inverted ? 'inverted' : ''}
-            ${googleSignIn ? 'google-sign-in' : ''}
-            custom-button
-        `} {...otherProps}>
+const CustomButton = ({ children, ...otherProps }) => (
+    <CustomButtonContainer {...otherProps}>
         {children}
-    </button>
+    </CustomButtonContainer>
 )
 
 export default CustomButton;
