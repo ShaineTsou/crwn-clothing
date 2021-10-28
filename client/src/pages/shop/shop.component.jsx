4eedf90127cec999a6c5376810296335fdd0dcd2
Shaine Tsou <shaineagro06@gmail.com>
2021-10-28 20:22:25 +0800

    used prettier

--- client/src/pages/shop/shop.component.jsx
+++ client/src/pages/shop/shop.component.jsx
@@ -1,35 +1,40 @@
-import React, { useEffect, lazy, Suspense } from 'react';
-import { Route } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import React, { useEffect, lazy, Suspense } from "react";
+import { Route } from "react-router-dom";
+import { useDispatch } from "react-redux";
 
-import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
+import { fetchCollectionsStart } from "../../redux/shop/shop.actions";
 
-import Spinner from '../../components/spinner/spinner.component';
+import Spinner from "../../components/spinner/spinner.component";
 
-const CollectionsOverviewContainer = lazy(() => import('../../components/collections-overview/collections-overview.container'));
-const CollectionPageContainer = lazy(() => import('../collection/collection.container'));
+const CollectionsOverviewContainer = lazy(() =>
+  import("../../components/collections-overview/collections-overview.container")
+);
+const CollectionPageContainer = lazy(() =>
+  import("../collection/collection.container")
+);
 
 const ShopPage = ({ match }) => {
-    const dispatch = useDispatch();
-    
-    useEffect(() => {
-        dispatch(fetchCollectionsStart());
-    }, [dispatch])
+  const dispatch = useDispatch();
 
-    return(            
-        <div className='shop-page'>
-            <Suspense fallback={<Spinner />}>
-                <Route 
-                    exact path={`${match.path}`} 
-                    component={CollectionsOverviewContainer}
-                />
-                <Route 
-                    path={`${match.path}/:collectionId`}
-                    component={CollectionPageContainer}
-                />
-            </Suspense>
-        </div>
-    );
+  useEffect(() => {
+    dispatch(fetchCollectionsStart());
+  }, [dispatch]);
+
+  return (
+    <div className="shop-page">
+      <Suspense fallback={<Spinner />}>
+        <Route
+          exact
+          path={`${match.path}`}
+          component={CollectionsOverviewContainer}
+        />
+        <Route
+          path={`${match.path}/:collectionId`}
+          component={CollectionPageContainer}
+        />
+      </Suspense>
+    </div>
+  );
 };
 
 export default ShopPage;
