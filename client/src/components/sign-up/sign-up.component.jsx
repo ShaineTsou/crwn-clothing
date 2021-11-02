b20be865b0451b68d5d032b536111e7495d2510b
Shaine Tsou <shaineagro06@gmail.com>
2021-11-02 22:37:13 +0800

    added requirements of sign up password

--- client/src/components/sign-up/sign-up.component.jsx
+++ client/src/components/sign-up/sign-up.component.jsx
@@ -1,83 +1,93 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 
-import FormInput from '../form-input/form-input.component';
-import CustomButton from '../custom-button/custom-button.component';
+import FormInput from "../form-input/form-input.component";
+import CustomButton from "../custom-button/custom-button.component";
 
-import { signUpStart } from '../../redux/user/user.actions';
+import { signUpStart } from "../../redux/user/user.actions";
 
-import { SignUpContainer, SignUpTitle } from './sign-up.styles';
+import { SignUpContainer, SignUpTitle } from "./sign-up.styles";
 
 const SignUp = () => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const [userCredentials, setUserCredentials] = useState({
-        displayName: '',
-        email: '',
-        password: '',
-        confirmPassword: ''
-    });
-    
-    const { displayName, email, password, confirmPassword } = userCredentials;
+  const [userCredentials, setUserCredentials] = useState({
+    displayName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
 
-    const handleChange = event => {
-        const { name, value } = event.target;
+  const { displayName, email, password, confirmPassword } = userCredentials;
 
-        setUserCredentials({ ...userCredentials, [name]: value });
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setUserCredentials({ ...userCredentials, [name]: value });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (password !== confirmPassword) {
+      alert("Passwords don't match");
+      return;
     }
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
+    const regex =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@$!%*?&])[A-Za-z0-9@$!%*?&]{8,}$/;
+    if (!regex.test(password)) {
+      alert(
+        "password should contain at least 8 characters, including at least 1 lower case letter, 1 upper case letter, 1 number and 1 special character"
+      );
+      return;
+    }
 
-        if (password !== confirmPassword) {
-            alert('Passwords don\'t match');
-            return;
-        }
-        dispatch(signUpStart({ email, password, displayName }));
-    };
+    dispatch(signUpStart({ email, password, displayName }));
+  };
 
-    return (
-        <SignUpContainer>
-            <SignUpTitle>I do not have an account</SignUpTitle>
-            <span>Sign up with your email and password</span>
-            
-            <form className='sign-up-form' onSubmit={handleSubmit}>
-                <FormInput 
-                    type='text'
-                    name='displayName'
-                    value={displayName}
-                    handleChange={handleChange}
-                    label='Display Name'
-                    required
-                />
-                <FormInput 
-                    type='email'
-                    name='email'
-                    value={email}
-                    handleChange={handleChange}
-                    label='Email'
-                    required
-                />
-                <FormInput 
-                    type='password'
-                    name='password'
-                    value={password}
-                    handleChange={handleChange}
-                    label='Password'
-                    required
-                />
-                <FormInput 
-                    type='password'
-                    name='confirmPassword'
-                    value={confirmPassword}
-                    handleChange={handleChange}
-                    label='Confirm Password'
-                    required
-                />
-                <CustomButton type='submit'>Sign Up</CustomButton>
-            </form>
-        </SignUpContainer>
-    )
-}
+  return (
+    <SignUpContainer>
+      <SignUpTitle>I do not have an account</SignUpTitle>
+      <span>Sign up with your email and password</span>
+
+      <form className="sign-up-form" onSubmit={handleSubmit}>
+        <FormInput
+          type="text"
+          name="displayName"
+          value={displayName}
+          handleChange={handleChange}
+          label="Display Name"
+          required
+        />
+        <FormInput
+          type="email"
+          name="email"
+          value={email}
+          handleChange={handleChange}
+          label="Email"
+          required
+        />
+        <FormInput
+          type="password"
+          name="password"
+          value={password}
+          handleChange={handleChange}
+          label="Password"
+          required
+        />
+        <FormInput
+          type="password"
+          name="confirmPassword"
+          value={confirmPassword}
+          handleChange={handleChange}
+          label="Confirm Password"
+          required
+        />
+        <CustomButton type="submit">Sign Up</CustomButton>
+      </form>
+    </SignUpContainer>
+  );
+};
 
 export default SignUp;
